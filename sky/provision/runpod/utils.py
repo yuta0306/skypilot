3f6a0d109e5ad85061257897768ecc09c1b1921e
yuta0306 <yuta.20010306@gmail.com>
2025-02-27 17:52:39 +0900

    fix: typo

--- sky/provision/runpod/utils.py
+++ sky/provision/runpod/utils.py
@@ -260,8 +260,7 @@ def launch(cluster_name: str, node_type: str, instance_type: str, region: str,
     setup_cmd = (
         'prefix_cmd() '
         '{ if [ $(id -u) -ne 0 ]; then echo "sudo"; else echo ""; fi; }; '
-        'echo "Exporting environment variables...";'
-        'printenv'
+        'printenv;'
         # 'printenv | grep -E \'^RUNPOD_|^PATH=|^_=\' | awk -F = \'{ print "export " $1 "=\"" $2 "\"" }\' >> /etc/rp_environment;'
         # 'echo "source /etc/rp_environment" >> ~/.bashrc;'
         '$(prefix_cmd) apt update;'
